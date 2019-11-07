ac3328246725b2af18a25f65c8441ec5d6bddf5e
gary-liu <498982703@qq.com>
2019-11-07 08:36:22 +0800

    java设计模式

---
+++ src/main/java/com/mimi/design/MimiDesignApplication.java
@@ -0,0 +1,13 @@
+package com.mimi.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimiDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MimiDesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mimi/design/MimiDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mimi.design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimiDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
